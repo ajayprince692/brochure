38a16b3dd5f122da713d1892eac0872df920c8c6
ajayprince <ajayprince1432@gmail.com>
2024-12-02 12:39:25 +0530

    second commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Brochure</h1>
-      <p className='mb-4 text-slate-700'>Brochure is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
-      <p className='mb-4 text-slate-700'>
-      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Brochure</h1>
+      <p className="mb-4 text-slate-700">
+        <b> Welcome to Brochure Real Estate</b>
+        At Brochure Real Estate, we specialize in helping clients buy, sell, and
+        rent properties in the most sought-after neighborhoods. Whether you're a
+        first-time buyer, an experienced investor, or someone looking for the
+        perfect rental, our expert team is here to guide you every step of the
+        way. 
+        <br />
+        <b>Our Services:</b> <br /><br />
+        <u>Buy:</u> Discover your dream home in the best locations. <br />
+        <u>Sell:</u> Get top dollar for your property with our strategic marketing and
+        expert advice. <br />
+        <u>Rent:</u> Find the ideal rental property to suit your
+        lifestyle and budget. <br />
+        <u>Exceptional Service:</u> We are committed to making the buying, selling, or
+        renting process as smooth and stress-free as possible. <br />
+        <u>Local Expertise:</u>We know the neighborhoods that matter most to you. Let us help you
+        navigate the real estate market with confidence.
       </p>
-      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      {/* <p className="mb-4 text-slate-700">
+        Our mission is to help our clients achieve their real estate goals by
+        providing expert advice, personalized service, and a deep understanding
+        of the local market. Whether you are looking to buy, sell, or rent a
+        property, we are here to help you every step of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents has a wealth of experience and knowledge in the real
+        estate industry, and we are committed to providing the highest level of
+        service to our clients. We believe that buying or selling a property
+        should be an exciting and rewarding experience, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p> */}
     </div>
-  )
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -65,7 +65,7 @@ export default function Home() {
           to={'/search'}
           className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
         >
-          Let's get started...
+          Let's goo...
         </Link>
       </div>
 
